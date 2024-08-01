1ba670438dc1c7b052a0f4ca88102ca969071ead
Jp-Cas <willy.casabona@gmail.com>
2024-07-31 20:49:54 -0500

    problema1.py

--- problema1.py
+++ problema1.py
@@ -1,7 +1,17 @@
 
 
 def main():
-    S       = input()
+    cadena   = input()
+    cadena_0 = ''
+    
+    for caracter in cadena:
+        if caracter.isalnum():
+            cadena_0 += caracter
+    
+    if cadena_0 == cadena_0[::-1]:
+        print('Es un palíndromo')
+    else:
+        print('No es un palíndromo')
     
 
 if __name__ == '__main__':
